5b4bdee647729d4808c94c35b36472fb85398856
tockards <tockards@ska.ac.za>
2018-10-17 13:30:46 +0200

    fixed ahead and ehind

--- test-git-script.py
+++ test-git-script.py
@@ -7,8 +7,7 @@ repo.remotes.origin.fetch()
 repo_name = repo.active_branch.name
 
 commits_behind = repo.iter_commits('{0}..origin/{0}'.format(repo_name))
-commits_ahead = repo.iter_commits('{0}..origin/{0}'.format(repo_name))
-
+commits_ahead = repo.iter_commits('origin/{0}../{0}'.format(repo_name))
 count_commits_ahead = sum(1 for c in commits_ahead)
 count_commits_behind = sum(1 for c in commits_behind)
 print ('commits ahead of origin {}'.format(count_commits_ahead))
